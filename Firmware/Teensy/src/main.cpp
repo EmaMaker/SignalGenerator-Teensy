37579c511c028936b4c99fe06a7f37b6e31bc362
EmaMaker <emamaker0@gmail.com>
2021-05-16 16:01:26 +0200

    firmware: add communication between 32u4 and teensy

--- Firmware/Teensy/src/main.cpp
+++ Firmware/Teensy/src/main.cpp
@@ -3,16 +3,85 @@
 #include "square.h"
 #include "sine.h"
 
+void communicate();
+void generate(int, int, int);
+
+bool receivingInfo, valid_flag;
+String s, tmp_type, tmp_frequency, tmp_duty;
+unsigned long frequency = 1000;
+int type = 0, duty = 50;
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
+  Serial1.begin(9600);
+  pinMode(21, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(21), communicate, RISING);
 
   setupWaves();
   setupSine();
   setupSquare();
 }
 
-FASTRUN void loop() {
-  generateSquareDAC1(1000000, 50);
+void loop() {
+  if(receivingInfo){
+    Serial1.write(42);  //The answer to life, universe and everything else
+    digitalWriteFast(LED_BUILTIN, HIGH);
+
+    delay(1000);
+    receivingInfo = false;
+    valid_flag = false;
+
+    while(!valid_flag){
+      s = Serial1.readStringUntil('W');
+      Serial.println(s);
+      //Check if it's a valid string, it should start with 'w'
+      if(!s.startsWith('w')) continue;
+
+      // Get type
+      tmp_type = s.substring(s.indexOf('t')+1, s.indexOf('T'));
+      type = tmp_type.toInt();
+      // Get duty
+      tmp_duty = s.substring(s.indexOf('d')+1, s.indexOf('D'));
+      duty = tmp_duty.toInt();
+      // Get frequency
+      tmp_frequency = s.substring(s.indexOf('f')+1, s.indexOf('F'));
+      frequency = tmp_frequency.toInt();
+
+      for(int i = 0; i < 100; i++) Serial1.write(69);
+      valid_flag = true;
+
+      Serial1.flush();
+
+      valid_flag = true;
+      Serial.print("Type: " );
+      Serial.println(type);
+      Serial.print("Duty: " );
+      Serial.println(duty);
+      Serial.print("Frequency: " );
+      Serial.println(frequency);
+      
+      digitalWriteFast(LED_BUILTIN, LOW);
+      generate(type, frequency, duty);
+    }
+  }
+}
+
+void communicate(){
+  receivingInfo = true;
+  stopGenerating();
+}
+
+void generate(int type, int frequency, int duty){
+  switch(type){
+    //sine
+    case 0:
+      generateSine(frequency);
+    break;
+    case 1:
+      generateSquareDAC1(frequency, duty);
+    break;
+
+  }
 }
